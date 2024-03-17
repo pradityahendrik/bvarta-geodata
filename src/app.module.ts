6931fa934cdbece564705b774419c0b8639ac0b0
pradityahendrik <pradityahendrik@gmail.com>
2024-03-17 14:50:55 +0700

    add db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './controllers/app.controller';
 import { FileController } from './controllers/file.controller';
 import { AppService } from './services/app.service';
 import { FileService } from './services/file.service';
+import config from '../database/config';
 
 @Module({
-  imports: [],
+  imports: [   
+    TypeOrmModule.forRoot(config),
+  ],
   controllers: [AppController, FileController],
   providers: [AppService, FileService],
 })
