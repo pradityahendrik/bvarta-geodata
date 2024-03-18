d0c5308fcec2363fcec70e581e20417c30a18e57
pradityahendrik <pradityahendrik@gmail.com>
2024-03-18 10:04:07 +0700

    swagger

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,11 +1,18 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from '../services/auth.service';
+import { ApiTags, ApiOperation, ApiResponse, ApiParam } from '@nestjs/swagger';
 
 @Controller()
+@ApiTags('Auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('/auth/login')
+  @ApiOperation({ summary: 'Upload a file' })
+  @ApiParam({ description: 'password', type: 'string', name: 'password' })
+  @ApiParam({ description: 'username', type: 'string', name: 'username' })
+  @ApiResponse({ status: 200, description: 'success' })
+  @ApiResponse({ status: 400, description: 'Error' })
   login(@Body() formData: any) {
     try {
         return this.authService.login(formData.username, formData.password);
--- src/controllers/file.controller.ts
+++ src/controllers/file.controller.ts
@@ -2,14 +2,27 @@ import { Controller, Post, UploadedFile, UseInterceptors, UseGuards } from '@nes
 import { FileInterceptor } from '@nestjs/platform-express/multer';
 import { FileService } from '../services/file.service';
 import { AuthGuard } from '../guard/auth.guard';
+import { ApiTags, ApiOperation, ApiResponse, ApiParam, ApiBearerAuth, ApiHeader } from '@nestjs/swagger';
 
 @Controller()
+@ApiTags('File')
 export class FileController {
   constructor(private readonly fileService: FileService) {}
 
   @Post('/file/upload')
   @UseGuards(AuthGuard)
   @UseInterceptors(FileInterceptor('file'))
+  @ApiOperation({ summary: 'Upload a file' })
+  @ApiParam({ description: 'File to upload', type: 'file', name: 'file' })
+  @ApiResponse({ status: 200, description: 'File uploaded successfully' })
+  @ApiResponse({ status: 400, description: 'No file uploaded' })
+  @ApiBearerAuth()
+  @ApiHeader({
+    name: 'Authorization',
+    description: 'Bearer <token>',
+    required: true,
+    example: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c',
+  })
   fileUpload(@UploadedFile() file) {
     try {
         if (!file) {
